ff0bce5c9be319468945a92500f302722c60c124
triesap <tyson@radroots.org>
2026-06-28 07:46:28 +0000

    Edit 404 page

--- src/routes/not_found.rs
+++ src/routes/not_found.rs
@@ -5,7 +5,7 @@ pub fn NotFound() -> impl IntoView {
     view! {
         <main class="page-main page-main-standalone">
             <section class="page-section">
-                <p class="page-text">"not found"</p>
+                <p class="page-text">"404 - No page"</p>
             </section>
         </main>
     }
